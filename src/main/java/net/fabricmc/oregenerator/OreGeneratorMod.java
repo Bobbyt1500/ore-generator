3a18c03ac3fd250a85dd9721c76948a29fd63b3e
Nick-Pena <npena1998@gmail.com>
2022-05-01 21:29:12 -0400

    Added cobble ore block

--- src/main/java/net/fabricmc/oregenerator/OreGeneratorMod.java
+++ src/main/java/net/fabricmc/oregenerator/OreGeneratorMod.java
@@ -1,6 +1,14 @@
 package net.fabricmc.oregenerator;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +18,17 @@ public class OreGeneratorMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("ore-generator");
 
+	public static final CobbleOre COBBLE_ORE = new CobbleOre(FabricBlockSettings.of(Material.STONE).hardness(4.0f));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		Registry.register(Registry.BLOCK, new Identifier("ore-generator", "cobble_ore"), COBBLE_ORE);
+		Registry.register(Registry.ITEM, new Identifier("ore-generator", "cobble_ore"),
+				new BlockItem(COBBLE_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		LOGGER.info("Hello Fabric world!");
 	}
